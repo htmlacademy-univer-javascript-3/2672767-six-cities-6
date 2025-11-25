0c45fd22da0e5df5b666dcd429cacc327cc14145
Aleksandr Vinichenko <vinich.yota@gmail.com>
2025-11-25 23:05:07 +0300

    Фикс путей

--- src/pages/favorites.tsx
+++ src/pages/favorites.tsx
@@ -7,7 +7,7 @@ const FavoritesPage: FC = () => (
         <div className="header__wrapper">
           <div className="header__left">
             <a className="header__logo-link" href="#">
-              <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
+              <img className="header__logo" src="/img/logo.svg" alt="6 cities logo" width="81" height="41"/>
             </a>
           </div>
           <nav className="header__nav">
@@ -51,7 +51,7 @@ const FavoritesPage: FC = () => (
                   </div>
                   <div className="favorites__image-wrapper place-card__image-wrapper">
                     <a href="#">
-                      <img className="place-card__image" src="img/apartment-small-03.jpg" width="150" height="110"
+                      <img className="place-card__image" src="/img/apartment-small-03.jpg" width="150" height="110"
                         alt="Place image"
                       />
                     </a>
@@ -87,7 +87,7 @@ const FavoritesPage: FC = () => (
                 <article className="favorites__card place-card">
                   <div className="favorites__image-wrapper place-card__image-wrapper">
                     <a href="#">
-                      <img className="place-card__image" src="img/room-small.jpg" width="150" height="110"
+                      <img className="place-card__image" src="/img/room-small.jpg" width="150" height="110"
                         alt="Place image"
                       />
                     </a>
@@ -134,7 +134,7 @@ const FavoritesPage: FC = () => (
                 <article className="favorites__card place-card">
                   <div className="favorites__image-wrapper place-card__image-wrapper">
                     <a href="#">
-                      <img className="place-card__image" src="img/apartment-small-04.jpg" width="150" height="110"
+                      <img className="place-card__image" src="/img/apartment-small-04.jpg" width="150" height="110"
                         alt="Place image"
                       />
                     </a>
@@ -173,8 +173,8 @@ const FavoritesPage: FC = () => (
       </div>
     </main>
     <footer className="footer container">
-      <a className="footer__logo-link" href="main.html">
-        <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33"/>
+      <a className="footer__logo-link" href="#">
+        <img className="footer__logo" src="/img/logo.svg" alt="6 cities logo" width="64" height="33"/>
       </a>
     </footer>
   </div>
--- src/pages/offer-not-logged.tsx
+++ src/pages/offer-not-logged.tsx
@@ -7,7 +7,7 @@ const OfferNotLoggedPage: FC = () => (
         <div className="header__wrapper">
           <div className="header__left">
             <a className="header__logo-link" href="#">
-              <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
+              <img className="header__logo" src="/img/logo.svg" alt="6 cities logo" width="81" height="41"/>
             </a>
           </div>
           <nav className="header__nav">
@@ -30,22 +30,22 @@ const OfferNotLoggedPage: FC = () => (
         <div className="offer__gallery-container container">
           <div className="offer__gallery">
             <div className="offer__image-wrapper">
-              <img className="offer__image" src="img/room.jpg" alt="Photo studio"/>
+              <img className="offer__image" src="/img/room.jpg" alt="Photo studio"/>
             </div>
             <div className="offer__image-wrapper">
-              <img className="offer__image" src="img/apartment-01.jpg" alt="Photo studio"/>
+              <img className="offer__image" src="/img/apartment-01.jpg" alt="Photo studio"/>
             </div>
             <div className="offer__image-wrapper">
-              <img className="offer__image" src="img/apartment-02.jpg" alt="Photo studio"/>
+              <img className="offer__image" src="/img/apartment-02.jpg" alt="Photo studio"/>
             </div>
             <div className="offer__image-wrapper">
-              <img className="offer__image" src="img/apartment-03.jpg" alt="Photo studio"/>
+              <img className="offer__image" src="/img/apartment-03.jpg" alt="Photo studio"/>
             </div>
             <div className="offer__image-wrapper">
-              <img className="offer__image" src="img/studio-01.jpg" alt="Photo studio"/>
+              <img className="offer__image" src="/img/studio-01.jpg" alt="Photo studio"/>
             </div>
             <div className="offer__image-wrapper">
-              <img className="offer__image" src="img/apartment-01.jpg" alt="Photo studio"/>
+              <img className="offer__image" src="/img/apartment-01.jpg" alt="Photo studio"/>
             </div>
           </div>
         </div>
@@ -126,7 +126,7 @@ const OfferNotLoggedPage: FC = () => (
               <h2 className="offer__host-title">Meet the host</h2>
               <div className="offer__host-user user">
                 <div className="offer__avatar-wrapper offer__avatar-wrapper--pro user__avatar-wrapper">
-                  <img className="offer__avatar user__avatar" src="img/avatar-angelina.jpg" width="74" height="74"
+                  <img className="offer__avatar user__avatar" src="/img/avatar-angelina.jpg" width="74" height="74"
                     alt="Host avatar"
                   />
                 </div>
@@ -154,7 +154,7 @@ const OfferNotLoggedPage: FC = () => (
                 <li className="reviews__item">
                   <div className="reviews__user user">
                     <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                      <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54"
+                      <img className="reviews__avatar user__avatar" src="/img/avatar-max.jpg" width="54" height="54"
                         alt="Reviews avatar"
                       />
                     </div>
@@ -189,7 +189,7 @@ const OfferNotLoggedPage: FC = () => (
             <article className="near-places__card place-card">
               <div className="near-places__image-wrapper place-card__image-wrapper">
                 <a href="#">
-                  <img className="place-card__image" src="img/room.jpg" width="260" height="200" alt="Place image"/>
+                  <img className="place-card__image" src="/img/room.jpg" width="260" height="200" alt="Place image"/>
                 </a>
               </div>
               <div className="place-card__info">
@@ -223,7 +223,7 @@ const OfferNotLoggedPage: FC = () => (
             <article className="near-places__card place-card">
               <div className="near-places__image-wrapper place-card__image-wrapper">
                 <a href="#">
-                  <img className="place-card__image" src="img/apartment-02.jpg" width="260" height="200"
+                  <img className="place-card__image" src="/img/apartment-02.jpg" width="260" height="200"
                     alt="Place image"
                   />
                 </a>
@@ -260,7 +260,7 @@ const OfferNotLoggedPage: FC = () => (
               </div>
               <div className="near-places__image-wrapper place-card__image-wrapper">
                 <a href="#">
-                  <img className="place-card__image" src="img/apartment-03.jpg" width="260" height="200"
+                  <img className="place-card__image" src="/img/apartment-03.jpg" width="260" height="200"
                     alt="Place image"
                   />
                 </a>
